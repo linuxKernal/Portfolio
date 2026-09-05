3eb2821b901975165c9144549610ff841e0eb455
Praveen P <lightsunfromsolar@gmail.com>
2026-09-05 19:45:28 +0530

    fix the link and enhance the empty link behaviour

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -25,37 +25,41 @@ export default function ProjectCard({
                 </div>
                 <div className="flex gap-4 mt-6">
                     <div className="flex gap-1 items-center">
-                        <a href={websiteLink} target="_blank" className="">
-                            <svg
-                                className="w-4 h-4"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <g stroke-width="0"></g>
-                                <g
+                        <svg
+                            className="w-4 h-4"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <g stroke-width="0"></g>
+                            <g
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                            ></g>
+                            <g>
+                                <path
+                                    d="M10.0464 14C8.54044 12.4882 8.67609 9.90087 10.3494 8.22108L15.197 3.35462C16.8703 1.67483 19.4476 1.53865 20.9536 3.05046C22.4596 4.56228 22.3239 7.14956 20.6506 8.82935L18.2268 11.2626"
+                                    stroke="black"
+                                    stroke-width="2.5"
                                     stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                ></g>
-                                <g>
-                                    <path
-                                        d="M10.0464 14C8.54044 12.4882 8.67609 9.90087 10.3494 8.22108L15.197 3.35462C16.8703 1.67483 19.4476 1.53865 20.9536 3.05046C22.4596 4.56228 22.3239 7.14956 20.6506 8.82935L18.2268 11.2626"
-                                        stroke="black"
-                                        stroke-width="2.5"
-                                        stroke-linecap="round"
-                                    ></path>
-                                    <path
-                                        d="M13.9536 10C15.4596 11.5118 15.3239 14.0991 13.6506 15.7789L11.2268 18.2121L8.80299 20.6454C7.12969 22.3252 4.55237 22.4613 3.0464 20.9495C1.54043 19.4377 1.67609 16.8504 3.34939 15.1706L5.77323 12.7373"
-                                        stroke="black"
-                                        stroke-width="2.5"
-                                        stroke-linecap="round"
-                                    ></path>
-                                </g>
-                            </svg>
+                                ></path>
+                                <path
+                                    d="M13.9536 10C15.4596 11.5118 15.3239 14.0991 13.6506 15.7789L11.2268 18.2121L8.80299 20.6454C7.12969 22.3252 4.55237 22.4613 3.0464 20.9495C1.54043 19.4377 1.67609 16.8504 3.34939 15.1706L5.77323 12.7373"
+                                    stroke="black"
+                                    stroke-width="2.5"
+                                    stroke-linecap="round"
+                                ></path>
+                            </g>
+                        </svg>
+                        <a
+                            href={websiteLink || "#"}
+                            target={websiteLink ? "_blank" : "_self"}
+                            className=""
+                        >
+                            <span className="text-md text-blue-600 hover:underline hover:decoration-1 cursor-default">
+                                website
+                            </span>
                         </a>
-                        <span className="text-md text-blue-600 hover:underline hover:decoration-1 cursor-default">
-                            website
-                        </span>
                     </div>
                     <div className="flex gap-1 items-center">
                         <img
@@ -64,8 +68,8 @@ export default function ProjectCard({
                             alt=""
                         />
                         <a
-                            href={sourceCode}
-                            target="_blank"
+                            href={sourceCode || "#"}
+                            target={sourceCode ? "_blank" : "_self"}
                             className="text-blue-600 hover:underline hover:decoration-1 cursor-default"
                         >
                             source code
